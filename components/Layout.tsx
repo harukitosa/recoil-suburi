78e1effd877f8fb6b5f80c86013b8f0e52c19d6d
harukitosa <13haruki28@gmail.com>
2021-05-10 21:10:48 +0900

    first commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
+import { RecoilRoot } from 'recoil'
 
 type Props = {
   children?: ReactNode
@@ -14,23 +14,9 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
+    <RecoilRoot>
+      {children}
+    </RecoilRoot>
     <footer>
       <hr />
       <span>I'm here to stay (Footer)</span>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,54 @@
 import Link from 'next/link'
+import { selector, useRecoilState, useRecoilValue } from 'recoil'
+import { atom } from 'recoil'
 import Layout from '../components/Layout'
+
+
+const textState = atom({
+  key: "textValue",
+  default: "hoge"
+})
+
+const charCountState = selector({
+  key: 'charCountState',
+  get: ({get}) => {
+    const text = get(textState);
+    return text.length;
+  }
+});
+
+const CharCount = () => {
+  const count = useRecoilValue(charCountState);
+  return (
+    <p>{count}</p>
+  );
+}
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event: { target: { value: string | ((currVal: string) => string) } }) => {
+    setText(event.target.value)
+  }
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+      <CharCount/>
+    </div>
+  );
+}
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
-      <Link href="/about">
+      <div>
         <a>About</a>
-      </Link>
+        <TextInput />
+      </div>
     </p>
   </Layout>
 )
